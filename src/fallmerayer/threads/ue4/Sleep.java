c1c607fc22f27e3a46086d785dc82406a317ac9f
Silas Demez <silasdemez@gmail.com>
2021-03-23 15:57:27 +0100

    started ue4

---
+++ src/fallmerayer/threads/ue4/Sleep.java
@@ -0,0 +1,9 @@
+package fallmerayer.threads.ue4;
+
+import java.util.Arrays;
+
+public class Sleep {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
