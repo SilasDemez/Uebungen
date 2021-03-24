b8985d52c97775fdd04ef91c78209c6283b4bfd9
Silas Demez <silasdemez@gmail.com>
2021-03-24 10:12:42 +0100

    threadds ue4

--- src/fallmerayer/threads/ue4/Sleep.java
+++ src/fallmerayer/threads/ue4/Sleep.java
@@ -1,9 +1,36 @@
 package fallmerayer.threads.ue4;
 
-import java.util.Arrays;
-
 public class Sleep {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
+
+    public static void main(String[] args) throws InterruptedException {
+        long timeToSleep = 0;       // in seconds
+        String temp;
+
+        for (String str:args) {
+            if (str.contains("m")){
+                //Minutes
+                temp = str.substring(0,str.indexOf("m"));
+                timeToSleep += Integer.parseInt(temp) * 60;
+            }else if (str.contains("h")){
+                //Hours
+                temp = str.substring(0,str.indexOf("h"));
+                timeToSleep += Integer.parseInt(temp) * 60 * 60;
+            }else if (str.contains("d")){
+                //Minutes
+                temp = str.substring(0,str.indexOf("d"));
+                timeToSleep += Integer.parseInt(temp) * 60 * 60 * 24;
+            }else{
+                //Minutes
+                try {
+                    temp = str.substring(0,str.indexOf("s"));
+                }catch (Exception e){
+                    temp = str;
+                }
+                timeToSleep += Integer.parseInt(temp);
+
+            }
+        }
+
+        Thread.sleep(timeToSleep * 1000);
     }
 }
