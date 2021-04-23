226072cc4f92c199d616f9837d4a70e3dd27b9e9
Silas Demez <silasdemez@gmail.com>
2021-04-23 12:03:22 +0200

    tried fixing - doeant work

--- src/fallmerayer/threads/threadpool/ConfrontAlgorithms.java
+++ src/fallmerayer/threads/threadpool/ConfrontAlgorithms.java
@@ -15,54 +15,68 @@ public class ConfrontAlgorithms {
     public static void random(int length){
         arr = new Integer[length];
         for (int i = 0; i < length; i++) {
-            arr[i] = (int) (Math.random() * 100000000);
+            arr[i] = (int) (Math.random() * 100);
         }
     }
 
     public static void sequentialCompare() {
+        double sum=0;
+        Integer[] clone = arr.clone();
         double startTime = System.nanoTime();
 
-        //System.out.println("Before Sort");
-        //System.out.println(Arrays.toString(confrontAlgorithms.arr));
+        System.out.println("Before Sort");
+        System.out.println(Arrays.toString(arr));
 
         mergeSort(arr, arr.length);
 
-        //System.out.println("After Sort");
-        //System.out.println(Arrays.toString(confrontAlgorithms.arr));
+        System.out.println("After Sort");
+        System.out.println(Arrays.toString(arr));
 
         double stopTime = System.nanoTime();
         double duration = (stopTime - startTime) / 1_000_000;  //divide by 1000000 to get milliseconds.
+        sum += duration;
         System.out.println("Execution Time mergeSort: " + duration + "ms");
 
+        arr = clone;
+        System.out.println("Array1: " + Arrays.toString(clone));
 
         startTime = System.nanoTime();
 
-        //System.out.println("Before Sort");
-        //System.out.println(Arrays.toString(confrontAlgorithms.arr));
+        System.out.println("Before Sort");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Array2: " + Arrays.toString(clone));
 
         heapSort(arr);
 
-        //System.out.println("After Sort");
-        //System.out.println(Arrays.toString(confrontAlgorithms.arr));
+        System.out.println("After Sort");
+        System.out.println(Arrays.toString(arr));
+
 
         stopTime = System.nanoTime();
         duration = (stopTime - startTime) / 1_000_000;  //divide by 1000000 to get milliseconds.
+        sum += duration;
         System.out.println("Execution Time heapSort: " + duration + "ms");
 
+        System.out.println("Array1: " + Arrays.toString(clone));
+        arr = clone;
 
         startTime = System.nanoTime();
 
-        //System.out.println("Before Sort");
-        //System.out.println(Arrays.toString(confrontAlgorithms.arr));
+        System.out.println("Before Sort");
+        System.out.println(Arrays.toString(arr));
 
         insertionSort(arr);
 
-        //System.out.println("After Sort");
-        //System.out.println(Arrays.toString(confrontAlgorithms.arr));
+        System.out.println("After Sort");
+        System.out.println(Arrays.toString(arr));
 
         stopTime = System.nanoTime();
         duration = (stopTime - startTime) / 1_000_000;  //divide by 1000000 to get milliseconds.
+        sum += duration;
         System.out.println("Execution Time insertionSort: " + duration + "ms");
+        System.out.println("Execution Time sequential: " + sum + "ms");
+
+        arr = clone;
     }
 
     public static void threadPool_invokeAll(){
@@ -125,7 +139,7 @@ public class ConfrontAlgorithms {
 
 
     public static void main(String[] args) {
-        random(10000000);
+        random(10);
         sequentialCompare();
         threadPool_invokeAll();
         threadPool_invokeAny();
